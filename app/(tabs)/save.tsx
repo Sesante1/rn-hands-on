2c7c100cc02f5d2eeefaceb90d370c62de1f9356
Sesante1 <jomelsesante989@email.com>
2025-06-26 20:53:03 +0800

    Initial commit

---
+++ app/(tabs)/save.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const save = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Save.</Text>
+        </View>
+    )
+}
+
+export default save
